6c282799ad5a367f402a1ab3d3f4806cb4122acc
Muhammad Anees Adrees <muhammadanis2007@gmail.com>
2019-12-29 19:24:34 +0500

    ADD APP DEVELOPER IN TITLE

--- javaspring-myapi/src/main/java/ids/myspringjava/controllers/HomeController.java
+++ javaspring-myapi/src/main/java/ids/myspringjava/controllers/HomeController.java
@@ -3,11 +3,13 @@ package ids.myspringjava.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class HomeController {
-	@GetMapping("/")
+	
+	@GetMapping("/home")
 	public String HomeWelcome(ModelMap model) {
 		
 		System.out.println("Welcome HOME");
@@ -16,5 +18,11 @@ public class HomeController {
 		
 		return "/home";
 	} 
+	
+	@RequestMapping("/")
+	public String index() {
+		return "Spring Boot Example!!";
+	}
+	
 	
 }
