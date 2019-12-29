216fe9031180e7dd9d75af88c38dc252e54c7043
Muhammad Anees Adrees IdeasDotSmart <muhammadanis2007@gmail.com>
2019-12-29 23:39:03 +0500

    Index.html added
    New file html 5 add

---
+++ javaspring-myapi/src/main/java/ids/myspringjava/controllers/IndexController.java
@@ -0,0 +1,24 @@
+package ids.myspringjava.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class IndexController {
+	
+	
+	@GetMapping ("/")
+	public String indexPage(ModelMap model) {
+	
+		return "index";
+	
+	
+	}
+	
+	
+	
+	
+}
