d316aca235f7168c5f5932fb976b087f61ff7f39
Muhammad Anees Adrees IdeasDotSmart <muhammadanis2007@gmail.com>
2019-12-30 00:43:46 +0500

    add updated controller
    Updated

--- javaspring-myapi/src/main/java/ids/myspringjava/controllers/IndexController.java
+++ javaspring-myapi/src/main/java/ids/myspringjava/controllers/IndexController.java
@@ -3,7 +3,7 @@ package ids.myspringjava.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 
 @Controller
@@ -15,10 +15,6 @@ public class IndexController {
 	
 		return "index";
 	
-	
 	}
-	
-	
-	
 	
 }
