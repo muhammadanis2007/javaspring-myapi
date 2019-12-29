3fb5ee9988bc9cc0d5d7b7b1d88ec9d22715a7a3
Muhammad Anees Adrees <muhammadanis2007@gmail.com>
2019-12-29 20:04:19 +0500

    saved fixedd

--- javaspring-myapi/src/main/java/ids/myspringjava/javaspringmyapi/JavaspringMyapiApplication.java
+++ javaspring-myapi/src/main/java/ids/myspringjava/javaspringmyapi/JavaspringMyapiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Configuration;;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
